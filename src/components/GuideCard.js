ca244e443097c2e731c5b3a13e52ed9a8e9540ee
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-04-29 23:05:12 +0500

    Done

--- src/components/GuideCard.js
+++ src/components/GuideCard.js
@@ -2,15 +2,19 @@ import React from "react";
 
 function GuideCard({ title, img }) {
   return (
-    <div className="bg-black pt-[30px] pb-[30px] xl:pb-[50px] px-[20px] text-center rounded-[14px]">
+    <div className="bg-black pt-[30px] pb-[30px] xl:pb-[50px] px-[20px] text-center rounded-[14px] group">
       <img
         src={img}
         className="w-full aspect-video object-contain mb-[20px] xl:mb-[50px] select-none pointer-events-none"
         alt=""
       />
-      <h1 className="text-white text-[18px] xl:text-[22px] font-bold text-center">
+      <h1 className="text-white text-[18px] xl:text-[22px] font-bold text-center mb-6">
         {title}
       </h1>
+
+      <div className="transition-all duration-[.3s] ease-[ease] overflow-hidden opacity-0 group-hover:opacity-[1]">
+        <p className="">Lorem ipsum dolor sit amet consectetur,</p>
+      </div>
     </div>
   );
 }
