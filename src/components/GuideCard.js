0fd658b3b73f93ff16d71f188608e81cb0b22a2a
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-04-29 23:08:15 +0500

    added show hide in GuideCard

--- src/components/GuideCard.js
+++ src/components/GuideCard.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function GuideCard({ title, img }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      menuRef.current.style.height = menuRef.current.scrollHeight + "px";
+    } else {
+      menuRef.current.style.height = 0 + "px";
+    }
+  }, [isOpen]);
+
   return (
-    <div className="bg-black pt-[30px] pb-[30px] xl:pb-[50px] px-[20px] text-center rounded-[14px] group">
+    <div
+      className="bg-black pt-[30px] pb-[30px] xl:pb-[50px] px-[20px] text-center rounded-[14px] group"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
       <img
         src={img}
         className="w-full aspect-video object-contain mb-[20px] xl:mb-[50px] select-none pointer-events-none"
@@ -12,7 +27,10 @@ function GuideCard({ title, img }) {
         {title}
       </h1>
 
-      <div className="transition-all duration-[.3s] ease-[ease] overflow-hidden opacity-0 group-hover:opacity-[1]">
+      <div
+        ref={menuRef}
+        className="h-0 overflow-hidden transition-all duration-[.3s] ease-[ease]"
+      >
         <p className="">Lorem ipsum dolor sit amet consectetur,</p>
       </div>
     </div>
